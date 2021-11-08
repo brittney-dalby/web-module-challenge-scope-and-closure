d36e41b7b1a125a4bcb4bdbe9b9ffc08de648e4d
Britt Dalby <91482954+brittney-dalby@users.noreply.github.com>
2021-11-08 14:02:44 -0700

    finished task 2

--- index.js
+++ index.js
@@ -69,10 +69,16 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * 3);
 }
 
+console.log('task 2',inning());
+console.log('task 2',inning());
+console.log('task 2',inning());
+console.log('task 2',inning());
+console.log('task 2',inning());
+console.log('task 2',inning());
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
