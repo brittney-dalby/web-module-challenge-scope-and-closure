a60bb7f02de3720fc5f514f670758e37af1532f8
Britt Dalby <91482954+brittney-dalby@users.noreply.github.com>
2021-11-08 13:37:50 -0700

    answered questions in task 1

--- index.js
+++ index.js
@@ -28,11 +28,18 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+  counter1 has a closure. there is another function inside the main function that looks to the main function.
   
   2. Which of the two uses a closure? How can you tell?
+
+  counter one uses a closure. The closure happens when the function inside the origional function looks to the main function.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+  counter 1 is preferable 
+
 */
 
 // counter1 code
